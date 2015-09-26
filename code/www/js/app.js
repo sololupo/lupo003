84d735df192d658502b6389c9a07b053439028bb
sololupo <sololupo.com@gmail.com>
2015-09-26 12:24:04 -0400

    first commit

--- code/www/js/app.js
+++ code/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('songhop', ['ionic', 'songhop.controllers'])
+angular.module('lupo', ['ionic', 'lupo.controllers', 'ionic-material'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -21,6 +21,8 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
 })
 
 
+
+  
 .config(function($stateProvider, $urlRouterProvider) {
 
   // Ionic uses AngularUI Router, which uses the concept of states.
@@ -30,12 +32,36 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
   $stateProvider
 
 
+  // Set up an abstract state for the splash directive:
+  .state('splash', {
+    url: '/',
+    templateUrl: 'templates/splash.html',
+    controller: 'SplashCtrl',
+    // if logged in, redirect to discover tab logged in
+    onEnter: function($state, User){
+      User.checkSession().then(function(hasSession) {
+        if (hasSession) $state.go('tab.discover');
+      });
+    }
+  })
+
   // Set up an abstract state for the tabs directive:
   .state('tab', {
     url: '/tab',
     abstract: true,
     templateUrl: 'templates/tabs.html',
-    controller: 'TabsCtrl'
+    controller: 'TabsCtrl',
+    // don't load the state until we've populated our User, if necessary
+    resolve: {
+      populateSession: function(User) {
+        return User.checkSession();
+      },
+      onEnter: function($state, User){
+        User.checkSession().then(function(hasSession) {
+          if (!hasSession) $state.go('splash');
+        });
+      }
+    }
   })
 
   // Each tab has its own nav history stack:
@@ -60,7 +86,7 @@ angular.module('songhop', ['ionic', 'songhop.controllers'])
       }
     })
   // If none of the above states are matched, use this as the fallback:
-  $urlRouterProvider.otherwise('/tab/discover');
+  $urlRouterProvider.otherwise('/');
 
 })
 
